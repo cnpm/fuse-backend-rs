48f35797372eec69faf9552cf7890d8168ce3b29
Wei Zhang <weizhang555.zw@gmail.com>
2024-01-04 19:26:09 +0800

    CI: add xfstests
    Add xfstests into CI for overlayfs, it will work as Github Actions.
    
    Currently not all test cases can be run, only about half of `generic/*`
    for the Fuse Overlay FS, some test cases are expected to fail due to
    missing function implementation, little are unexpected due to potential
    bugs which will be investigated laterly.
    
    Signed-off-by: Wei Zhang <weizhang555.zw@gmail.com>

--- tests/scripts/xfstests_overlay.sh
+++ tests/scripts/xfstests_overlay.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 sudo apt-get install acl attr automake bc dbench dump e2fsprogs fio gawk \
     gcc git indent libacl1-dev libaio-dev libcap-dev libgdbm-dev libtool \
     libtool-bin liburing-dev libuuid1 lvm2 make psmisc python3 quota sed \
-    uuid-dev uuid-runtime xfsprogs linux-headers-$(uname -r) sqlite3 \
-    exfatprogs f2fs-tools ocfs2-tools udftools xfsdump \
+    uuid-dev uuid-runtime xfsprogs linux-headers-$(uname -r) sqlite3
+sudo apt-get install exfatprogs f2fs-tools ocfs2-tools udftools xfsdump \
     xfslibs-dev
 
 # clone xfstests and install.
@@ -18,31 +18,35 @@ make
 sudo make install
 # overwrite local config.
 cat >local.config  <<EOF
-export TEST_DEV=testpassthrough
-export TEST_DIR=/tmp/pathr_dst
+export TEST_DEV=testoverlay
+export TEST_DIR=/tmp/testoverlay/merged
+#export SCRATCH_DEV=testoverlay
+#export SCRATCH_MNT=/tmp/test2/merged
 export FSTYP=fuse
-export FUSE_SUBTYP=.testpassthrough
+export FUSE_SUBTYP=.testoverlay
 EOF
 
 # create fuse overlay mount script.
 # /tmp/testoverlay must exists.
-sudo cat >/usr/sbin/mount.fuse.testpassthrough  <<EOF
+sudo cat >/usr/sbin/mount.fuse.testoverlay  <<EOF
 #!/bin/bash
 
 ulimit -n 1048576
-exec /usr/sbin/passthrough /tmp/pathr_src /tmp/pathr_dst \
-    1>>/tmp/testpassthrough.log 2>&1 &
+exec /usr/sbin/overlay -o \
+    lowerdir=/tmp/testoverlay/lower2:/tmp/testoverlay/lower1,upperdir=/tmp/testoverlay/upper,workdir=/tmp/testoverlay/work \
+    testoverlay /tmp/testoverlay/merged \
+    1>>/tmp/testoverlay.log 2>&1 &
 sleep 1
 EOF
-sudo chmod +x /usr/sbin/mount.fuse.testpassthrough
+sudo chmod +x /usr/sbin/mount.fuse.testoverlay
 
-# create related dirs.
-mkdir -p /tmp/pathr_src /tmp/pathr_dst
+# create related directories.
+mkdir -p /tmp/testoverlay/{upper,work,merged,lower2,lower1}
 
 echo "====> Start to run xfstests."
 # run tests.
 cd /tmp/xfstests-dev
 # Some tests are not supported by fuse or cannot pass currently.
-sudo ./check -fuse -E $current_dir/xfstests_pathr.exclude
+sudo ./check -fuse -E $current_dir/xfstests_overlay.exclude
 
 
